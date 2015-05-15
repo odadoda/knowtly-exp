f575629a48d986b8c9708e6eb13fdf02f31a356b
Øyvind Dahl <oda@enonic.com>
2015-05-15 16:43:22 +0200

    Cleaning up.

--- src/main/resources/cms/assets/js/knowtly.js
+++ src/main/resources/cms/assets/js/knowtly.js
@@ -1,4 +1,3 @@
-;(function ( $ ){
 
 /*
 *	knowtly v1
@@ -7,30 +6,103 @@
 */
 
 
-
-$.fn.knowtly = function( options ){
+;(function ( $, window, document, undefined ){
 	
-	var stuff = $(this);
-	var opts = $.extend({
-		mainForm: this,
-		mainWrapper: this.find('#commandLineWrapper'),
-		mainCommandLine: this.find('input[type="search"]'),
-		commands: {
-			new : function(){
-				console.log('pewpew newnew');		
+	var pluginName = "knowtly";
+	var me = {};
+	
+	var defaults = {
+    	mainForm: this,
+        commandlist: $('<ol class="' + pluginName + '_commandlist" ></ol>')
+	}; 
+	
+	var commands = {
+    	'new' : function(args){
+        	openEditorView(args);
+    	} 
+	}
+	
+	
+	// construcor
+	function Knowtly( element, options ){
+        this.element = element;
+        this.options = $.extend( {}, defaults, options ); 
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.commands = commands;
+        this.init();   	
+	}
+	
+	
+	Knowtly.prototype.init = function(){
+        console.log(this);  
+        
+        //disaable the submit button
+        $(this.element).on('submit', function(e){
+            e.preventDefault();
+            me.executeCommand();
+        });
+                 
+        // trigger on input
+        $(this.element).find('input[type="search"]').on( 'input', $(this).find('input[type="search"]'), this.filterInput );	
+        
+        
+        
+        
+        console.log('####');
+        console.log('#### Initialization done. You may precede with caution ####');
+        console.log('####');
+	}
+	
+	
+	$.fn[pluginName] = function( options ){
+    	return this.each(function(){
+        	if( !$.data(this, 'plugin_' + pluginName) ){
+            	me = new Knowtly(this, options);
+            	$.data(this, 'plugin_' + pluginName, me);
+        	}
+    	});
+    }
+	
+	
+	// checks if input is a command or a search 
+	Knowtly.prototype.filterInput = function(event){
+    	if( typeof(commands[this.value]) == 'function' ){
+			me.formatToCommandAppearance(this);
+		}else {
+    		me.search(this);
+		}
+    };
+    
+    
+    // changes input text to a listitem in "valid commands"
+	Knowtly.prototype.formatToCommandAppearance = function(inputElement){
+        var commandItem = $('<li>'+inputElement.value+'</li>');
+        me.options.commandlist.append(commandItem);
+        $(inputElement).val('');	
+	};
+	
+	
+	// executes the first "comand" in commandlist, passing on arguments
+	Knowtly.prototype.executeCommand = function(){
+    	me.commands[me.options.commandlist.children('li').eq(0).html()]();
+	};
+	
+	
+	Knowtly.prototype.search = function( inputElement ){
+        /*form.attr('data-requestcount', parseInt(form.attr('data-requestcount')) + 1);
+		$.post( form.attr('action'), { 'q': form.children('[name="q"]').val(), 'requestid': form.attr('data-requestcount') }, function( data ){
+			if( parseInt($(data).attr('data-for-request')) >= form.attr('data-requestcount')){
+				$('.js-note-list').html( data );
 			}
-		}}, options);
+		});*/		
+	}
 	
-	stuff.data('knowtly.opts', opts);
-	
-	var api = $.fn.knowtly.API;
-	
+	/*
 	opts.mainForm.submit(function(event){
 		event.preventDefault();
 		//window.URL = '#' + opts.mainCommandLine.val();
 	});
-		
-	
 		
 		
 	opts.mainCommandLine.on("input", function(){
@@ -39,23 +111,12 @@ $.fn.knowtly = function( options ){
 		}	
 	});	
 	
-	return this;
-}
-
-
-$.fn.knowtly.API = {
 	
-	
-	
-	
-	
-	/*	command	*/
-	changeToCommand: function(){
+    changeToCommand: function(){
 		console.log(this.opts());		
 	},
 	
 	
-	/*	send search request */
 	search: function(){
 		form.attr('data-requestcount', parseInt(form.attr('data-requestcount')) + 1);
 		$.post( form.attr('action'), { 'q': form.children('[name="q"]').val(), 'requestid': form.attr('data-requestcount') }, function( data ){
@@ -63,7 +124,7 @@ $.fn.knowtly.API = {
 				$('.js-note-list').html( data );
 			}
 		});	
-	}	
-}
+	}*/
 
-}(jQuery));
+}(jQuery, window, document));
+
